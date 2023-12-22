960515e951976da4a338c4e51baee050b02e61cb
INTERNAL\viktorija.nebylica <viktorija.nebylica@visma.com>
2023-12-22 15:56:11 +0000

    add video and image

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function media() {
+  const video = document.querySelector(".background-video");
+  const image = document.querySelector(".background-image");
+
+  if (window.innerWidth < 600) {
+    if (video) video.remove();
+  } else {
+    if (image) image.remove();
+  }
+}
+
 function pastEventButtons() {
   const pastEventCard = document.querySelectorAll(".past-event");
   pastEventCard.forEach((item) => {
@@ -15,3 +26,4 @@ function pastEventButtons() {
 }
 
 pastEventButtons();
+media();
